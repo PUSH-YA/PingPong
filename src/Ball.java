a9ae9d3e07bf75a13b5acbc6888995f4283c9e29
PUSH-YA <pushya22.jain@gmail.com>
2022-03-22 22:51:44 -0700

    try1

---
+++ src/Ball.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle{
+    
+    Random random;
+    int xVelocity;
+    int yVelocity;
+
+    Ball(){
+
+    }
+    public void setXDirection(int xDir){
+
+    }
+    public void setYDirection(int yDir){
+
+    }
+    public void move(){
+
+    }
+    public void draw(Graphics g){
+
+    }
+}
